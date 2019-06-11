83f933dbeb82e0ae3500af79cb98687529fc96b2
eoinokeeffe_mudano <eoin.okeeffe@mudano.com>
2019-06-11 13:59:12 +0100

    updating so can work from ubuntu for creating docker image

--- tools/run.sh
+++ tools/run.sh
@@ -2,6 +2,15 @@
 
 set -o errexit
 
+# set environment variables
+export OAUTH_GOOGLE2_KEY=$1
+export OUTH_GOOGLE2_SECRET=$2
+export DOCCANO_DB_NAME=$3
+export DOCCANO_DB_USER=$4
+export DOCCANO_DB_PASSWD=$5
+export DOCCANO_DB_HOST=$6
+export DOCCANO_DB_PORT=$7
+
 if [[ ! -d "app/staticfiles" ]]; then python app/manage.py collectstatic --noinput; fi
 
 python app/manage.py wait_for_db
